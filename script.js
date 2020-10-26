9f1fcbd4aede8151aedfb1a6e4eb698efa7a61d6
khaleds5769 <73479632+khaleds5769@users.noreply.github.com>
2020-10-26 09:34:21 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
-let ownerName = "Your Name"
-let userName = "YourUsername";
+let ownerName = "Khaled S"
+let userName = "khaleds5769";
 
 document.querySelectorAll(".owner-name").forEach((e) => {
     e.innerHTML = ownerName;
